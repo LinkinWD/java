f07b3c6f78e69c3f866f1e6a8875fca5ffb7ed58
yaram <yaramodesu@gmail.com>
2020-11-24 10:51:30 +0200

    testataan

---
+++ src/main/java/linkinwd/koulu1/firstProject.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkinwd.koulu1;
+
+/**
+ *
+ * @author yaram
+ */
+public class firstProject {
+    public static void main(String[] args) {
+        
+    }
+            
+}
