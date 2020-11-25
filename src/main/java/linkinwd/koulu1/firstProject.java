574063918b54f6483ce9c16f12e1bb85f152a8aa
yaram <yaramodesu@gmail.com>
2020-11-25 14:48:38 +0200

    aloitus toimii, nyt tulee vaikeeta :)

--- src/main/java/linkinwd/koulu1/firstProject.java
+++ src/main/java/linkinwd/koulu1/firstProject.java
@@ -3,15 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+        
 package linkinwd.koulu1;
 
 /**
  *
  * @author yaram
  */
+import java.util.Scanner;
 public class firstProject {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("******************************************");
+        System.out.println("Tervetuloa luomaan uutta tiliä pankillemme");
+        System.out.println("******************************************");
+        System.out.println("Saisinko etunimesi?");
+        String etuNimi = input.nextLine();
+        System.out.println("Saisinko sukunimesi?");
+        String sukuNimi = input.nextLine();
+        System.out.println("Minkä ikäinen olet??");
+        int ika = Integer.valueOf(input.nextLine());
+        System.out.println("Saisinko sähköpostiosoitteesi?");
+        String sahkoPosti= input.nextLine();
+        
+        
     }
+        
             
 }
